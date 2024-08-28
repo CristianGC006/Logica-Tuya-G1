b69b7ccabdf890d278d8f948951d2e8c7942b19b
CristianGC006 <cogolloguzmanesteban@gmail.com>
2024-08-28 16:15:29 -0500

    Update Imc.java

--- ApuntesLogicaTuyaSA/src/ejercicios/Imc.java
+++ ApuntesLogicaTuyaSA/src/ejercicios/Imc.java
@@ -1,7 +1,26 @@
 package ejercicios;
-
+import java.util.Scanner;
 public class Imc {
     public static void main(String[] args) {
+    float   peso;
+    float altura;
+    Scanner sc =new Scanner(System.in);
+        System.out.println("Ingrese su peso");
+        peso= sc.nextFloat();
+        System.out.println("ingrese su altura");
+        altura=sc.nextFloat();
+        System.out.println("Un momento por favor :)");
+
+        float imc;
+
+        imc=peso/(altura*altura);
+        System.out.println("Su imc es de: \n"+imc);
+
+
+
+
+
+
 
     }
 }
